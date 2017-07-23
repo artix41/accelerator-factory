68fbc228a7a3216172836981ea172f47582666cf
Arthur Pesah <arthur.pesah@gmail.com>
2017-07-23 15:01:04 +0200

    Add empty state

---
+++ src/states/emptyState.js
@@ -0,0 +1,12 @@
+export function EmptyState(game){
+    console.log("Starting empty state");
+};
+
+EmptyState.prototype = {
+    preload: function(){
+
+    },
+    create: function(){
+
+    }
+};
